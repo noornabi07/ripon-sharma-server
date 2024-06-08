356318edfe26c57b271f6bad1b46377d7c250368
noornabi07 <noornabiprogram07@gmail.com>
2024-06-08 21:17:33 +0600

    insert data portfolio in database

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cnuoch3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,23 +30,55 @@ async function run() {
 
     const reviewCollection = client.db("riponDb").collection("reviews");
     const teamCollection = client.db("riponDb").collection("teams");
+    const portfolioCollection = client.db("riponDb").collection("portfolio");
 
     // Review setting start here
-    app.get('/review', async(req, res) =>{
-        const result = await reviewCollection.find().toArray();
-        res.send(result);
+    app.get('/review', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
     })
 
-    app.post('/addreview', async(req, res) =>{
+    app.post('/addreview', async (req, res) => {
       const newReview = req.body;
       const result = await reviewCollection.insertOne(newReview);
       res.send(result);
     })
     // Review setting end here
 
-    app.get('/team', async(req, res) =>{
-        const result = await teamCollection.find().toArray();
-        res.send(result);
+    // portfolio setting start here
+
+    app.get('/portfolio', async (req, res) => {
+      const result = await portfolioCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/portfolio/:text', async (req, res) => {
+      if (req.params.text == "marketing" || req.params.text == "youtube" || req.params.text == "seo") {
+        const result = await portfolioCollection.find({ category: req.params.text }).toArray();
+        return res.send(result)
+      }
+      const result = await portfolioCollection.find({}).toArray();
+      res.send(result);
+    })
+
+    app.get('/port/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await portfolioCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.post('/addportfolio', async(req, res) =>{
+      const newPortfolio = req.body;
+      const result = await portfolioCollection.insertOne(newPortfolio);
+      res.send(result);
+    })
+
+    // portfolio setting end here
+
+    app.get('/team', async (req, res) => {
+      const result = await teamCollection.find().toArray();
+      res.send(result);
     })
 
 
@@ -61,10 +93,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res) =>{
-    res.send("Ripon sharma is coming..")
+app.get('/', (req, res) => {
+  res.send("Ripon sharma is coming..")
 })
 
-app.listen(port, () =>{
-    console.log(`ripon sharma project running port: ${port}`);
+app.listen(port, () => {
+  console.log(`ripon sharma project running port: ${port}`);
 })
